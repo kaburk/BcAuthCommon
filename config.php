7e28e22d13e28f544e87c82bd5e893c0c5e8d891
kaburk <github@kaburk.com>
2026-04-15 00:31:43 +0900

    fix documents

--- config.php
+++ config.php
@@ -3,6 +3,6 @@ return [
     'type' => 'Plugin',
     'title' => __d('baser_core', '認証共通基盤'),
     'description' => __d('baser_core', '追加認証プラグイン向けの共通ログイン・リダイレクト処理を提供します。'),
-    'author' => 'baserCMS',
-    'url' => 'https://basercms.net/',
+    'author' => 'kaburk',
+    'url' => 'https://blog.kaburk.com/',
 ];
